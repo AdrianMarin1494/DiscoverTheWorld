269f243032b59db1f76ecf3759645cbf9aa5c442
AdrianMarin1494 <marin.adrian1494@gmail.com>
2023-06-05 23:30:04 +0300

    added more details on the modal, added a title, changed the name of the app

--- front_end_discover_the_world/src/app/components/DetailsModal.tsx
+++ front_end_discover_the_world/src/app/components/DetailsModal.tsx
@@ -4,8 +4,8 @@ const DetailsModal = (props) => {
 
     return (
         <div style={{
-            "width": "30vw", 
-            "height": "20vh", 
+            "width": "50vw", 
+            "height": "30vh", 
             "position": "fixed",
             "marginLeft": "-10%",
             "zIndex": "100px", 
@@ -13,9 +13,9 @@ const DetailsModal = (props) => {
         }}>
             <h3>Country: {props.countryName} {props.flag}</h3>
             <h3>The Capital is {props.capitalName}</h3>
-            <h3>It is located on {props.regionName}</h3>
+            <h3>It is located on {props.regionName} Continent</h3>
             <p>Borders: {props.borders}</p>
-            <p>Languages: {props.languages}</p>
+            <p>Languages spoken: {props.languages}</p>
             <a href={props.map} target="_blank">Click to see the map</a>
             <br />
             <button onClick={props.onClose}>Close</button>
--- front_end_discover_the_world/src/app/pages/CountriesPage.tsx
+++ front_end_discover_the_world/src/app/pages/CountriesPage.tsx
@@ -3,6 +3,8 @@ import CountriesData from "../components/CountriesData";
 const CountriesPage = () => {
     return (
         <div>
+            <h2>Discover the countries</h2>
+            <br />
             <CountriesData />
         </div>
     );
