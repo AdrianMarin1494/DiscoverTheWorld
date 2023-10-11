ac701fe0baa4dc224559c92ead579b725d3710b5
AdrianMarin1494 <marin.adrian1494@gmail.com>
2023-10-11 11:41:48 +0300

    new

--- front_end_discover_the_world/src/app/pages/MainPage.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-const MainPage = () => {
-    return (
-        <h3>Welcome</h3>
-    );
-};
-
-export default MainPage;
--- front_end_discover_the_world/src/app/services/getCountries.ts
+++
@@ -1,9 +0,0 @@
-export const getCountries = async () => {
-    try {
-        const response = await fetch("https://restcountries.com/v3.1/all");
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        throw new Error("Failed to fetch the countries data");
-    }
-};
