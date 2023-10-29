10d47847fa8ad4996e1239962926059d5b4e849e
AdrianMarin1494 <marin.adrian1494@gmail.com>
2023-10-29 22:03:39 +0200

    added API call

---
+++ discover_the_world_frontend/src/app/components/CountriesTable.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+const CountriesTable = () => {
+    const [countriesData, setCountriesData] = useState<any[] | null>(null);
+    async function getCountries() {
+        try {
+            const res: Response = await fetch("https://restcountries.com/v3.1/all");
+            const data: any[] = await res.json();
+            console.log(data);
+            if (data) {
+                const listedData = data.map((country) => {
+                    return (
+                        <tr key={country["name"]["common"]}>
+                            <td>{country["name"]["common"]}</td>
+                            <td>{country["region"]}</td>
+                        </tr>
+                    );
+                })
+                setCountriesData(listedData);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        getCountries();
+    }, []);
+
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Capital</th>
+                        <th>Region</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {countriesData}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default CountriesTable;
---
+++ discover_the_world_frontend/src/app/components/pages/MainPage.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+
+import CountriesTable from "../CountriesTable";
+
+const MainPage = () => {
+    return (
+        <div>
+            <h3>Main Page</h3>
+            <CountriesTable />
+        </div>
+    );
+};
+
+export default MainPage;
