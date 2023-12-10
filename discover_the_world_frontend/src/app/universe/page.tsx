695186d94dcb35f133bd90a940b3f8ab472cd5b2
AdrianMarin1494 <marin.adrian1494@gmail.com>
2023-12-10 16:43:33 +0200

    added previous button and ordered the data

--- discover_the_world_frontend/src/app/universe/page.tsx
+++ discover_the_world_frontend/src/app/universe/page.tsx
@@ -22,9 +22,13 @@ function getCurrentDate() {
     return formatDate;
 }
 
-function subtractDaysFromDate(dateString: string, days: number) {
+function subtractDaysFromDate(dateString: string, days: number, isSubtracting: boolean) {
     const inputDate = new Date(dateString);
-    inputDate.setDate(inputDate.getDate() - days);
+    if (isSubtracting) {
+        inputDate.setDate(inputDate.getDate() - days);
+    } else {
+        inputDate.setDate(inputDate.getDate() + days);
+    }
 
     const year = inputDate.getFullYear();
     const month = (inputDate.getMonth() + 1).toString().padStart(2, '0');
@@ -44,7 +48,7 @@ const Universe: React.FC = () => {
         url: "",
     }]);
 
-    const [startDate, setStartDate] = useState<string>(subtractDaysFromDate(getCurrentDate(), 4));
+    const [startDate, setStartDate] = useState<string>(subtractDaysFromDate(getCurrentDate(), 4, true));
     const [endDate, setEndDate] = useState<string>(getCurrentDate());
     const [isLoading, setIsLoading] = useState<boolean>(true);
 
@@ -53,6 +57,14 @@ const Universe: React.FC = () => {
         try {
             const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=RjoPcnI4Nad2ovYEQ7nr9O4kmf48EgDulMJLfVRJ&start_date=${starting}&end_date=${ending}`);
             const data = await res.json();
+            data.sort((a: any, b: any) => {
+                if (a["date"] < b["date"]) {
+                    return 1;
+                } else if (a["date"] > b["date"]) {
+                    return -1;
+                }
+                return 0
+            })
             console.log(data);
             setPhotos(data);
         } catch(err) {
@@ -66,9 +78,9 @@ const Universe: React.FC = () => {
         getData(startDate, endDate);
     }, [])
 
-    function handleNext() {
-        let newStartDate: string = subtractDaysFromDate(startDate, 5);
-        let newEndDate: string = subtractDaysFromDate(endDate, 5);
+    function handleNextOrBack(isNext: boolean) {
+        let newStartDate: string = subtractDaysFromDate(startDate, 5, isNext);
+        let newEndDate: string = subtractDaysFromDate(endDate, 5, isNext);
         getData(newStartDate, newEndDate);
         setStartDate(newStartDate);
         setEndDate(newEndDate);
@@ -76,7 +88,7 @@ const Universe: React.FC = () => {
     
     return (
         <div>
-            <h3>Astronomy</h3>
+            <h3>Enjoy the Astronomy</h3>
             {isLoading ? 
                 <div>
                     <h3>Loading...</h3>
@@ -84,9 +96,8 @@ const Universe: React.FC = () => {
                 : 
                 <div style={{display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center"}}>
                     {photos.map((photo) => {
-                        console.log(photo);
                         return (
-                            <div>
+                            <div key={photo.title}>
                                 <h3>{photo.title}</h3>
                                 <h4>By: {photo.copyright}</h4>
                                 <h5><i>{photo.date}</i></h5>
@@ -98,7 +109,8 @@ const Universe: React.FC = () => {
                     )}
                 </div>
             }
-            <button onClick={handleNext} style={{width: "4vw", height: "4vh"}}>Next</button>
+            <button onClick={() => handleNextOrBack(false)} style={{width: "4vw", height: "4vh"}}>Previous</button>
+            <button onClick={() => handleNextOrBack(true)} style={{width: "4vw", height: "4vh"}}>Next</button>
         </div>
     );
 };
